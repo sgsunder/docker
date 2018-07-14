21d6c2852bb61658f918746894339bda6763525e
Shyam Sunder <sgsunder1@gmail.com>
2018-07-14 16:08:44 -0400

    reverseproxy: Added ability to change hostname

--- reverseproxy/etc/cont-init.d/20-generate-configuration.sh
+++ reverseproxy/etc/cont-init.d/20-generate-configuration.sh
@@ -7,14 +7,18 @@ skeleton_file="/etc/nginx/conf.d/skel.conf"
 while read input; do
   # Parse input arguments
   xscript_prefix=$(echo $input | cut -d '|' -f 1)
-  origin_port=$(echo $input | cut -d '|' -f 2)
+  origin=$(echo $input | cut -d '|' -f 2)
   origin_prefix=$(echo $input | cut -d '|' -f 3)
+
+  origin_host=$(echo $origin | cut -d ':' -f 1)
+  origin_port=$(echo $origin | cut -d ':' -f 2)
 
   # Log what I'm doing
   echo "  $xscript_prefix >> localhost:$origin_port$origin_prefix"
 
   # Generate configuration from skel.conf
   sed -e "s#@XScriptName@#$xscript_prefix#g" \
+      -e "s#@OriginHost@#$origin_host#" \
       -e "s#@OriginPort@#$origin_port#" \
       -e "s#@OriginPrefix@#$origin_prefix#" \
       $skeleton_file >> $output_file
